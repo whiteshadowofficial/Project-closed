943bfad9370abf38861c1493438729f3e683defc
sreezz-ai <85176770+sreezz-ai@users.noreply.github.com>
2021-07-13 16:04:16 +0530

    Update _alpha.js

--- plugins/_alpha.js
+++ plugins/_alpha.js
@@ -123,7 +123,7 @@ if (Config.WORKTYPE == 'private') {
 }
 else if (Config.WORKTYPE == 'public') {
 
-    Asena.addCommand({pattern: 'asena ?(.*)', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+    Asena.addCommand({pattern: 'alpha ?(.*)', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
 
         var CMD_HELP = '';
         if (match[1] === '') {
