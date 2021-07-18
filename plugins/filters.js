acbbfafca4cc801ef6eaa0263d105d789b87d086
sreezz-ai <85176770+sreezz-ai@users.noreply.github.com>
2021-07-18 12:19:33 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -11,7 +11,6 @@ const {MessageType, Mimetype } = require('@adiwajshing/baileys');
 const FilterDb = require('./sql/filters');
 const Config = require('../config')
 
-const jid = Config.DISBGM != false ? Config.DISBGM.split(',') : []
 const Language = require('../language');
 const Lang = Language.getString('filters');
 
