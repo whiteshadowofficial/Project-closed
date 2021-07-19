c38272189cd0c25d600ffa967185b233cb67c5de
sreezz-ai <85176770+sreezz-ai@users.noreply.github.com>
2021-07-19 12:03:01 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -6,28 +6,18 @@ you may not use this file except in compliance with the License.
 WhatsAsena - Yusuf Usta
 */
 
+const fs = require('fs')
 const Asena = require('../events');
-const {MessageType} = require('@adiwajshing/baileys');
+const {MessageType, Mimetype } = require('@adiwajshing/baileys');
 const FilterDb = require('./sql/filters');
-const Config = require('../config')
+
 const Language = require('../language');
 const Lang = Language.getString('filters');
 
-var f_rep = ''
-if (Config.LANG == 'TR') f_rep = '*Filtre Ayarlandı ✅*'
-if (Config.LANG == 'EN') f_rep = '*Filter Setted ✅*'
-if (Config.LANG == 'AZ') f_rep = '*Filtr Düzəldildi ✅*'
-if (Config.LANG == 'ES') f_rep = '*Filtro Ajustado ✅*'
-if (Config.LANG == 'HI') f_rep = '*फ़िल्टर सेट ✅*'
-if (Config.LANG == 'RU') f_rep = '*Фильтр настроен ✅*'
-if (Config.LANG == 'ML') f_rep = '*ഫിൽട്ടർ സെറ്റ് ✅*'
-if (Config.LANG == 'ID') f_rep = '*Filter Set ✅*'
-if (Config.LANG == 'PT') f_rep = '*Filtro Ajustado ✅*'
+Asena.addCommand({pattern: 'filter ?(.*)', fromMe: true, desc: Lang.FILTER_DESC, dontAddCommandList: true}, (async (message, match) => {
+    match = match[1].match(/[\'\"\“](.*?)[\'\"\“]/gsm);
 
-Asena.addCommand({pattern: 'filter ?(.*)', fromMe: true, desc: Lang.FILTER_DESC}, (async (message, match) => {
-    Mat = match[1].match(/[\'\"\“](.*?)[\'\"\“]/gsm);
-
-    if (Mat === null) {
+    if (match === null) {
         filtreler = await FilterDb.getFilter(message.jid);
         if (filtreler === false) {
             await message.client.sendMessage(message.jid,Lang.NO_FILTER,MessageType.text)
@@ -36,19 +26,16 @@ Asena.addCommand({pattern: 'filter ?(.*)', fromMe: true, desc: Lang.FILTER_DESC}
             filtreler.map((filter) => mesaj += '```' + filter.dataValues.pattern + '```\n');
             await message.client.sendMessage(message.jid,mesaj,MessageType.text);
         }
-    } else if (message.reply_message && match[1] !== '') {
-        await FilterDb.setFilter(message.jid, match[1].replace(/['"“]+/g, ''), message.reply_message.text);
-        return await message.client.sendMessage(message.jid,f_rep,MessageType.text);
     } else {
-        if (Mat.length < 2) {
-            return await message.client.sendMessage(message.jid,Lang.NEED_REPLY + ' ```.filter "test" "test two"',MessageType.text);
+        if (match.length < 2) {
+            return await message.client.sendMessage(message.jid,Lang.NEED_REPLY + ' ```.filter "sa" "as"',MessageType.text);
         }
-        await FilterDb.setFilter(message.jid, Mat[0].replace(/['"“]+/g, ''), Mat[1].replace(/['"“]+/g, '').replace(/[#]+/g, '\n'), Mat[0][0] === "'" ? true : false);
-        await message.client.sendMessage(message.jid,Lang.FILTERED.format(Mat[0].replace(/['"]+/g, '')),MessageType.text);
+        await FilterDb.setFilter(message.jid, match[0].replace(/['"“]+/g, ''), match[1].replace(/['"“]+/g, '').replace(/[#]+/g, '\n'), match[0][0] === "'" ? true : false);
+        await message.client.sendMessage(message.jid,Lang.FILTERED.format(match[0].replace(/['"]+/g, '')),MessageType.text);
     }
 }));
 
-Asena.addCommand({pattern: 'stop ?(.*)', fromMe: true, desc: Lang.STOP_DESC}, (async (message, match) => {
+Asena.addCommand({pattern: 'stop ?(.*)', fromMe: true, desc: Lang.STOP_DESC, dontAddCommandList: true}, (async (message, match) => {
     match = match[1].match(/[\'\"\“](.*?)[\'\"\“]/gsm);
     if (match === null) {
         return await message.client.sendMessage(message.jid,Lang.NEED_REPLY + '\n*Example:* ```.stop "hello"```',MessageType.text)
@@ -62,31 +49,25 @@ Asena.addCommand({pattern: 'stop ?(.*)', fromMe: true, desc: Lang.STOP_DESC}, (a
         await message.client.sendMessage(message.jid,Lang.DELETED, MessageType.text)
     }
 }));
-
-
 Asena.addCommand({on: 'text', fromMe: false}, (async (message, match) => {
-    var filtreler = await FilterDb.getFilter(message.jid);
-    if (!filtreler) return; 
-    return filtreler.map(
-        async (filter) => {
-            pattern = new RegExp(filter.dataValues.regex ? filter.dataValues.pattern : ('\\b(' + filter.dataValues.pattern + ')\\b'), 'gm');
-            if (message.message == filter.dataValues.pattern) {
-                await new Promise(r => setTimeout(r, 900));
-                return await message.client.sendMessage(message.jid,filter.dataValues.text, MessageType.text, {quoted: message.data});
-            }
+        if (!!message.mention && message.mention[0] == '918921483992@s.whatsapp.net') {
+await message.client.sendMessage(message.jid, fs.readFileSync('./uploads/mention.mp3'), MessageType.audio, { mimetype: Mimetype.mp4Audio, quoted : message.data, ptt: true})
         }
-    );
-}));
-Asena.addCommand({on: 'text', fromMe: true, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {
+const array = ['.alpha']
+array.map( async (a) => {
+let pattern = new RegExp(`\\b${a}\\b`, 'g');
+if(pattern.test(message.message)){
+       await message.client.sendMessage(message.jid, fs.readFileSync('./uploads/' + a + '.mp3'), MessageType.audio, { mimetype: Mimetype.mp4Audio, quoted: message.data, ptt: true})
+}
+});
+
     var filtreler = await FilterDb.getFilter(message.jid);
     if (!filtreler) return; 
-    return filtreler.map(
+    filtreler.map(
         async (filter) => {
             pattern = new RegExp(filter.dataValues.regex ? filter.dataValues.pattern : ('\\b(' + filter.dataValues.pattern + ')\\b'), 'gm');
-            var fo = message.message.replace('$', '')
-            if (fo == filter.dataValues.pattern && message.message.startsWith('$')) {
-                await new Promise(r => setTimeout(r, 100));
-                return await message.client.sendMessage(message.jid,filter.dataValues.text, MessageType.text, {quoted: message.data});
+            if (pattern.test(message.message)) {
+                await message.client.sendMessage(message.jid,filter.dataValues.text, MessageType.text, {quoted: message.data});
             }
         }
     );
