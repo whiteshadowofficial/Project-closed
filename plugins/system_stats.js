cb709b5591242a4d7ade9ee66b6f36dc428a3885
ThevinduWA <87611806+ThevinduWA@users.noreply.github.com>
2021-07-23 21:31:19 +0530

    Update system_stats.js

--- plugins/system_stats.js
+++ plugins/system_stats.js
@@ -30,7 +30,7 @@ Asena.addCommand({pattern: 'alive', fromMe: false, desc: Lang.ALIVE_DESC}, (asyn
     }
 }));
 
-Asena.addCommand({pattern: 'sysd', fromMe: false, desc: Lang.SYSD_DESC}, (async (message, match) => {
+Asena.addCommand({pattern: 'sysd', fromMe: true, desc: Lang.SYSD_DESC}, (async (message, match) => {
     const child = spawnSync('neofetch', ['--stdout']).stdout.toString('utf-8')
     await message.sendMessage(
         '```' + child + '```', MessageType.text
