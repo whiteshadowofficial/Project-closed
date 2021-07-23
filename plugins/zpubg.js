3d3aeb514034ac59be0fcb1c1c4b5e2e6f07ab1a
ThevinduWA <87611806+ThevinduWA@users.noreply.github.com>
2021-07-23 09:21:45 +0530

    Update zpubg.js

--- plugins/zpubg.js
+++ plugins/zpubg.js
@@ -23,7 +23,7 @@ if (Config.WORKTYPE == 'private') {
 
 else if (Config.WORKTYPE == 'public') {
 
-    Asena.addCommand({ pattern: 'zpubg ?(.*)', fromMe: false,dontAddCommandList: true}, (async (message, match) => {
+    Asena.addCommand({ pattern: 'zpubg ?(.*)', fromMe: true,dontAddCommandList: true}, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(need);
 
