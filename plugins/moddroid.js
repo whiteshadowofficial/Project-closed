12b29f97229cf7355d115ca8209cac122d35d392
sreezz-ai <85176770+sreezz-ai@users.noreply.github.com>
2021-08-01 15:55:07 +0530

    Update moddroid.js

--- plugins/moddroid.js
+++ plugins/moddroid.js
@@ -12,7 +12,7 @@ const Config = require('../config');
 const Language = require('../language');
 const Lang = Language.getString('weather');
 
-Asena.addCommand({pattern: 'modd ?(.*)', fromMe: false, desc:"gives mod apps." }, async (message, match) => {
+Asena.addCommand({pattern: 'modd ?(.*)', fromMe: false, desc: "gives mod apps." }, async (message, match) => {
 	if (match[1] === '') return await message.client.sendMessage(message.jid, '```Give me a name.```', MessageType.text, { quoted: message.data });
 	let url = 'https://api.lolhuman.xyz/api/moddroid?apikey=6d8f65061357712bad246857&query=${match[1]}&plot=full'
 	        const response = await got(url);
