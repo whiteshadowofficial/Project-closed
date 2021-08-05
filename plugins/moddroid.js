c2889201656e0378e5a7656e1aae821ba3dc60a3
sreezz-ai <85176770+sreezz-ai@users.noreply.github.com>
2021-08-05 13:28:48 +0530

    Update and rename moddroid,js to moddroid.js

--- plugins/moddroid.js
+++ plugins/moddroid.js
@@ -22,5 +22,6 @@ Asena.addCommand({ pattern: 'modd ?(.*)', fromMe: false, desc: "Finds mod verssi
     
         }
     
-   await message.client.sendMessage(message.jid,ini_txt,MessageType.text, {quoted: message.data });    
+   await message.client.sendMessage(message.jid,ini_txt,MessageType.text, {quoted: message.data });  
+    
 }));   
