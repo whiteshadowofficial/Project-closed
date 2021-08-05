214e8f325f099e87d7799519fba33de740b1a2be
sreezz-ai <85176770+sreezz-ai@users.noreply.github.com>
2021-08-05 17:04:41 +0530

    Update moddroid.js

--- plugins/moddroid.js
+++ plugins/moddroid.js
@@ -6,6 +6,7 @@ WhatsAsenaDuplicated*/
 const Asena = require('../events')
 const { MessageType } = require('@adiwajshing/baileys')
 const got = require('got');
+const { fetchJson, getBuffer } = require('./fetcher')
 
 const Language = require('../language')
 const Lang = Language.getString('search')
