9e305fb1f9f521088921544fb8e9d329a2224345
sreezz-ai <85176770+sreezz-ai@users.noreply.github.com>
2021-08-05 10:04:12 +0530

    Create moddroid.js

--- plugins/moddroid.js
+++ plugins/moddroid.js
@@ -4,15 +4,18 @@ you may not use this file except in compliance with the License.
 WhatsAsenaDuplicated
 */
 
-const Asena = require('../events');
-const {MessageType} = require('@adiwajshing/baileys');
+const Asena = require('../events')
+const { MessageType } = require('@adiwajshing/baileys')
 const got = require('got');
-const Config = require('../config');
+const cn = require('../config');
+const { fetchJson, getBuffer } = require('./fetcher')
 
-const Language = require('../language');
-const Lang = Language.getString('weather');
+const Language = require('../language')
+const Lang = Language.getString('weather')
 
-Asena.addCommand({ pattern: 'mod ?(.*)', fromMe: false, desc: Lang.USAGE,  deleteCommand: false }, async (message, match) => {
+
+if (cn.WORKTYPE == 'private') {
+  Asena.addCommand({ pattern: 'modd ?(.*)', fromMe: true, desc: Lang.USAGE,  deleteCommand: false }, async (message, match) => {
 
     if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORDS,MessageType.text);    
     var reply = await message.client.sendMessage(message.jid,Lang.GET_MODD,MessageType.text, {quoted: message.data});
@@ -28,3 +31,59 @@ Asena.addCommand({ pattern: 'mod ?(.*)', fromMe: false, desc: Lang.USAGE,  delet
     await message.client.sendMessage(message.jid,ini_txt,MessageType.text, {quoted: message.data});
     await reply.delete();
   })
+
+  Asena.addCommand({ pattern: 'spo ?(.*)', fromMe: true, desc: Lang.USAGE,  deleteCommand: false }, async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORDS,MessageType.text);    
+    var reply = await message.client.sendMessage(message.jid,Lang.GET_MODD,MessageType.text, {quoted: message.data});
+
+    get_result = await fetchJson(`https://api.lolhuman.xyz/api/spotifysearch?apikey=qamdi5652&query=${match[1]}`)
+                    get_result = get_result.result
+                    ini_txt = ""
+                    for (var x of get_result) {
+                        ini_txt += `Title : ${x.title}\n`
+                        ini_txt += `Artists : ${x.artists}\n`
+                        ini_txt += `Link : ${x.link}\n\n`
+                    }
+
+    await message.client.sendMessage(message.jid,ini_txt,MessageType.text, {quoted: message.data});
+    await reply.delete();
+  })
+}
+else if (cn.WORKTYPE == 'public') {
+  Asena.addCommand({ pattern: 'modd ?(.*)', fromMe: false, desc: Lang.USAGE }, async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORDS,MessageType.text);    
+    var reply = await message.client.sendMessage(message.jid,Lang.GET_MODD,MessageType.text, {quoted: message.data});
+
+    get_result = await fetchJson(`https://api.lolhuman.xyz/api/moddroid?apikey=qamdi5652&query=${match[1]}`)
+                    get_result = get_result.result
+                    ini_txt = ""
+                    for (var x of get_result) {
+                        ini_txt += `Name : ${x.name}\n`
+                        ini_txt += `Link : ${x.link}\n\n`
+                    }
+
+    await message.client.sendMessage(message.jid,ini_txt,MessageType.text, {quoted: message.data});
+    await reply.delete();
+  })
+
+  Asena.addCommand({ pattern: 'spo ?(.*)', fromMe: false, desc: Lang.USAGE,  deleteCommand: false }, async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORDS,MessageType.text);    
+    var reply = await message.client.sendMessage(message.jid,Lang.GET_MODD,MessageType.text, {quoted: message.data});
+
+    get_result = await fetchJson(`https://api.lolhuman.xyz/api/spotifysearch?apikey=qamdi5652&query=${match[1]}`)
+                    get_result = get_result.result
+                    ini_txt = ""
+                    for (var x of get_result) {
+                        ini_txt += `Title : ${x.title}\n`
+                        ini_txt += `Artists : ${x.artists}\n`
+                        ini_txt += `Link : ${x.link}\n\n`
+                    }
+
+    await message.client.sendMessage(message.jid,ini_txt,MessageType.text, {quoted: message.data});
+    await reply.delete();
+  })
+
+}
