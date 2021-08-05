5c5ae1efb71a4c644c86b8f5661ff74ff3ae8c6d
sreezz-ai <85176770+sreezz-ai@users.noreply.github.com>
2021-08-05 12:48:00 +0530

    Update moddroid.js

--- plugins/moddroid.js
+++ plugins/moddroid.js
@@ -12,7 +12,7 @@ const Lang = Language.getString('weather')
 
 Asena.addCommand({ pattern: 'modd ?(.*)', fromMe: false, desc: "Finds mod verssion." },  (async (message, match) => {
     if (match[1] === '') return await message.client.sendMessage(message.jid, '```Give me a app name.```', MessageType.text, { quoted: message.data });       
-    let url = `https://api.lolhuman.xyz/api/moddroid?apikey=qamdi5652&query=${match[1]}`
+    let url = `https://api.lolhuman.xyz/api/moddroid?apikey=qamdi5652&query=${match[1]}&plot=full`
     const response = await got(url);
     const json = JSON.parse(response.body);
     if (json.Response != 'True') return await message.client.sendMessage(message.jid, '*Not found.*', MessageType.text, { quoted: message.data });
