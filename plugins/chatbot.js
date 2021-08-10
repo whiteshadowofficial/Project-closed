4f9929c7c5b4564f8f080c11081a016db8d0364f
chirutheyi <88527773+chiruthey@users.noreply.github.com>
2021-08-10 10:28:11 +0530

    Update chatbot.js

--- plugins/chatbot.js
+++ plugins/chatbot.js
@@ -34,7 +34,7 @@ else if (Config.WORKTYPE == 'public') {
 	try {
 		const response = await got(url);
 		const json = JSON.parse(response.body);
-	  if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '\n\nAlpha Bot:' + Lang.BOT +' ```' + json.messages[0].response + '```\n\n' , MessageType.text,{quoted: message.data});
+	  if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '\nAlpha Bot: ' + Lang.BOT +' ```' + json.messages[0].response + '```\n\n' , MessageType.text,{quoted: message.data});
 	} catch {
 		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDAC, MessageType.text);
 	}
