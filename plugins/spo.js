a678488738c9387aa299c8d9b908ed372981e584
chirutheyi <88527773+chiruthey@users.noreply.github.com>
2021-08-25 18:50:02 +0530

    Update spo.js

--- plugins/spo.js
+++ plugins/spo.js
@@ -13,7 +13,7 @@ Asena.addCommand({ pattern: 'spotify ?(.*)', fromMe: false, desc: "Download song
          if (!link) return await message.client.sendMessage(message.jid, '```Give me a link```',MessageType.text)
     
         await axios
-          .get(`https://api.lolhuman.xyz/api/spotify?apikey=queenamdipublic&url=${link}`)
+          .get(`https://api.lolhuman.xyz/api/spotify?apikey=https://github.com/BlackAmda&url=${link}`)
           .then(async (response) => {
             const {
               link,
