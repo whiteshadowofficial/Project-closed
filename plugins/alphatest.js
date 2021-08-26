b32f36bbb487d5b5d916a5cbd5bb2d881d303707
chirutheyi <88527773+chiruthey@users.noreply.github.com>
2021-08-26 18:35:08 +0530

    Update alphatest.js

--- plugins/alphatest.js
+++ plugins/alphatest.js
@@ -33,7 +33,7 @@ if (Config.WORKTYPE == 'private') {
                     } else {
                         HANDLER = '.';
                     }
-                    CMD_HELP += '*🎯 ' + Lang.COMMAND + ':* ' + (match.length >= 3 ? (HANDLER + match[2]) : command.pattern) + (command.desc === '' ? '\n\n' : '\n');
+                    CMD_HELP += '*🎯 ' + Lang.COMMAND + ':* ' + (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + (command.desc === '' ? '\n\n' : '\n');
                     if (command.desc !== '') CMD_HELP += '*📝 ' + Lang.DESC + ':* ' + command.desc + (command.warn === '' ? '\n\n' : '\n');
                     if (command.usage !== '') CMD_HELP += '*⌨️ ' + Lang.EXAMPLE + ':* ' + command.usage + '\n\n';
                     if (command.warn !== '') CMD_HELP += '*⚠️ ' + Lang.WARN + ':* ' + command.warn + '\n\n';
@@ -108,7 +108,7 @@ else if (Config.WORKTYPE == 'public') {
                     } else {
                         HANDLER = '.';
                     }
-                    CMD_HELP += '*🎯 ' + Lang.COMMAND + ':* ' + (match.length >= 3 ? (HANDLER + match[2]) : command.pattern) + (command.desc === '' ? '\n\n' : '\n');
+                    CMD_HELP += '*🎯 ' + Lang.COMMAND + ':* ' + (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + (command.desc === '' ? '\n\n' : '\n');
                     if (command.desc !== '') CMD_HELP += '*📝 ' + Lang.DESC + ':* ' + command.desc + (command.warn === '' ? '\n\n' : '\n');
                     if (command.usage !== '') CMD_HELP += '*⌨️ ' + Lang.EXAMPLE + ':* ' + command.usage + '\n\n';
                     if (command.warn !== '') CMD_HELP += '*⚠️ ' + Lang.WARN + ':* ' + command.warn + '\n\n';
