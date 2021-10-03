8e080e13b0ed5a314f77ca79195de7d77b0066d8
Avneeth Kaur <90620251+xmain12@users.noreply.github.com>
2021-10-03 08:54:43 +0530

    Update scrapers.js

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -986,7 +986,7 @@ else if (config.WORKTYPE == 'public') {
     
         var mesaj = '';
         arama.all.map((video) => {
-            mesaj += '```�?' + video.title + '``` �? ' + video.url + '\n\n'
+            mesaj += '```✅' + video.title + '``` ✅ ' + video.url + '\n\n'
         });
 
         await message.client.sendMessage(message.jid,mesaj,MessageType.text);
