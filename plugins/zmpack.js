ed21661309338fce9eae3a40a1b65683a59a77e8
Avneeth Kaur <90620251+xmain12@users.noreply.github.com>
2021-10-04 16:12:33 +0530

    Update zmpack.js

--- plugins/zmpack.js
+++ plugins/zmpack.js
@@ -1,9 +1,3 @@
-/* Copyright (C) 2021 TENUX-Neotro.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-NEOTROX - TEENUHX
-*/
-
 const Asena = require('../events');
 const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
 const fs = require('fs');
