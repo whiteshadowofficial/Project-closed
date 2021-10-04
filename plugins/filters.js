c5bc2ff33f3d9c2e1850a1f2bf3e229c90d1d6e4
Avneeth Kaur <90620251+xmain12@users.noreply.github.com>
2021-10-04 18:53:53 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -1,9 +1,3 @@
-/* Copyright (C) 2021 TENUX-Neotro.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-NEOTROX - TEENUHX
-*/
-
 const fs = require('fs')
 const Asena = require('../events');
 const {MessageType, Mimetype } = require('@adiwajshing/baileys');
