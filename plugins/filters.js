3f444d1234a28e8b851a06786dd161d31976ecb1
Avneeth Kaur <90620251+xmain12@users.noreply.github.com>
2021-10-07 10:32:40 +0530

    new eroo fixed🤘

--- plugins/filters.js
+++ plugins/filters.js
@@ -184,7 +184,7 @@ Asena.addCommand({on: 'text', fromMe: false}, (async (message, match) => {
     if (!!message.mention && message.mention[0] == '15862077024@s.whatsapp.net') {
 await message.client.sendMessage(message.jid, fs.readFileSync('./stickers/mention.webp'), MessageType.sticker, { mimetype: Mimetype.webp, quoted : message.data, ptt: false})
     }
-const array = [hi','gn','gm','ane','gmmac','kawada','love','mk','mkna','modi','na','sorry','umma','hm','hmm','mmm','report','ai','mn','mokadda','moko','dukai','adalei','list']
+const array = ['hi','gn','gm','ane','gmmac','kawada','love','mk','mkna','modi','na','sorry','umma','hm','hmm','mmm','report','ai','mn','mokadda','moko','dukai','adalei','list']
 array.map( async (a) => {
 let pattern = new RegExp(`\\b${a}\\b`, 'g');
 if(pattern.test(message.message)){
