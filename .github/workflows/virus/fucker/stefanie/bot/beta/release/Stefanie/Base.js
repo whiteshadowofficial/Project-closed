8d436f49afe80f6ac99bbf56d634fe9d3d28376e
@VirusFucker <90620251+xmain12@users.noreply.github.com>
2021-10-23 07:50:01 +0530

    Stefanie V 9.2 beta release✅

---
+++ .github/workflows/virus/fucker/stefanie/bot/beta/release/Stefanie/Base.js
@@ -0,0 +1,21 @@
+/* Copyright (C) 2021 ws virus Fucker.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+VIRUS - FUCKER
+*/
+
+'use strict';
+
+class Base {
+    constructor(client) {
+        Object.defineProperty(this, 'client', { value: client });
+    }
+
+    _clone() {
+        return Object.assign(Object.create(this), this);
+    }
+    
+    _patch(data) { return data; }
+}
+
+module.exports = Base;
