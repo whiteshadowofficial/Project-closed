328e52cf66db1f2af4684db2b95a47ec03e9e696
@VirusFucker <90620251+xmain12@users.noreply.github.com>
2021-11-04 05:25:32 +0530

    Update StringSession.js

--- whatsasena/StringSession.js
+++ whatsasena/StringSession.js
@@ -28,7 +28,7 @@ class StringSession {
     }
 
     createStringSession(dict) {
-        return 'STEFANIE;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'ASENA;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
     }
 }
 
